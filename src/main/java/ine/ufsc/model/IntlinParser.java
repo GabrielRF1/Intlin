9ad2d61493dea8ec6a50600a05713a68a132c891
GabrielRF1 <rasferreiramilde@gmail.com>
2021-10-27 16:56:09 -0300

    Passa no teste aprimorado para alternatives

--- src/main/java/ine/ufsc/model/IntlinParser.java
+++ src/main/java/ine/ufsc/model/IntlinParser.java
@@ -11,7 +11,9 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -32,22 +34,50 @@ public class IntlinParser implements DictParser {
                 PreparedStatement stm = con.prepareStatement("INSERT OR "
                         + "IGNORE INTO Word(word, word_class, gender) "
                         + "VALUES(?, ?, ?)");
+                JSONArray alternatives = null;
+                JSONArray definitions = null;
                 for (String keyStr : jsonObject.keySet()) {
                     Object keyvalue = jsonObject.get(keyStr);
                     switch (keyStr) {
                         case "word":
                         case "word_class":
                         case "gender":
-                            int index = keyStr.equals("word") ? 1 : 
-                                    keyStr.equals("word_class") ? 2 : 3;
+                            int index = keyStr.equals("word") ? 1
+                                    : keyStr.equals("word_class") ? 2 : 3;
                             stm.setString(index, (String) keyvalue);
-                        default:
                             break;
+                        case "alt":
+                            alternatives = (JSONArray) keyvalue;
+                            break;
+                        case "defs":
+                            definitions = (JSONArray) keyvalue;
+                            break;
+                        default:
                     }
                 }
                 stm.execute();
+                if (alternatives != null) {
+                    parseAlternatives(alternatives, con);
+                }
             }
-            //System.out.println("");
+        }
+    }
+
+    private void parseAlternatives(JSONArray alternatives, Connection con) throws SQLException {
+        Statement stm = con.createStatement();
+        ResultSet resSet = stm.executeQuery("select last_insert_rowid() as word_id");
+        int word_id = resSet.getInt("word_id");
+        System.out.println("WORD_ID: " + word_id);
+        for (Object alternative : alternatives) {
+            PreparedStatement prepStem = con
+                    .prepareStatement("INSERT OR IGNORE INTO Extension VALUES (?)");
+            prepStem.setString(1, (String) alternative);
+            prepStem.execute();
+            PreparedStatement prepStem2 = con
+                    .prepareStatement("INSERT OR IGNORE INTO Alternative VALUES (?, ?)");
+            prepStem2.setInt(1, word_id);
+            prepStem2.setString(2, (String) alternative);
+            prepStem2.execute();
         }
     }
 }
