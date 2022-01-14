d04a84d85df9772f08a0a467c437d0cfa4bcdba5
GabrielRF1 <rasferreiramilde@gmail.com>
2022-01-14 10:34:13 -0300

    JMDICT bd schema and a initial test

--- src/test/java/ine/ufsc/model/dictionaries/parsers/JMDictParserTest.java
+++ src/test/java/ine/ufsc/model/dictionaries/parsers/JMDictParserTest.java
@@ -5,13 +5,17 @@
  */
 package ine.ufsc.model.dictionaries.parsers;
 
+import static ine.ufsc.model.dictionaries.parsers.CCCedictParserTest.con;
 import java.io.File;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -32,16 +36,81 @@ public class JMDictParserTest {
 
     private static void setUpDB() throws ClassNotFoundException, SQLException {
         Class.forName("org.sqlite.JDBC");
-        con = DriverManager.getConnection("jdbc:sqlite:testDict"+File.separator+"dbJMDictTest.db");
+        con = DriverManager.getConnection("jdbc:sqlite:testDict" + File.separator + "dbJMDictTest.db");
         Statement stm = con.createStatement();
         //create tables
+        stm.execute("CREATE TABLE IF NOT EXISTS Word("
+                + "word_id INTEGER PRIMARY KEY)");
+        stm = con.createStatement();
+        stm.execute("CREATE TABLE IF NOT EXISTS RElement("
+                + "r_id INTEGER PRIMARY KEY,"
+                + "priority INTEGER,"
+                + "reading STRING NOT NULL,"
+                + "word_id INTEGER NOT NULL,"
+                + "FOREIGN KEY(word_id) REFERENCES Word(word_id))");
+        stm = con.createStatement();
+        stm.execute("CREATE TABLE IF NOT EXISTS KElement("
+                + "k_id INTEGER PRIMARY KEY,"
+                + "priority INTEGER,"
+                + "kanji STRING NOT NULL,"
+                + "word_id INTEGER NOT NULL,"
+                + "FOREIGN KEY(word_id) REFERENCES Word(word_id))");
+        stm = con.createStatement();
+        stm.execute("CREATE TABLE IF NOT EXISTS Definition("
+                + "def_id INTEGER PRIMARY KEY,"
+                + "type STRING,"
+                + "word_id INTEGER NOT NULL,"
+                + "FOREIGN KEY(word_id) REFERENCES Word(word_id))");
+        stm = con.createStatement();
+        stm.execute("CREATE TABLE IF NOT EXISTS PartOfSpeech("
+                + "pos_id INTEGER PRIMARY KEY,"
+                + "pos STRING NOT NULL)");
+        stm = con.createStatement();
+        stm.execute("CREATE TABLE IF NOT EXISTS Gloss("
+                + "gloss_id INTEGER PRIMARY KEY,"
+                + "gloss STRING NOT NULL,"
+                + "def_id INTEGER NOT NULL,"
+                + "FOREIGN KEY(def_id) REFERENCES Definition(def_id))");
+        stm = con.createStatement();
+        stm.execute("CREATE TABLE IF NOT EXISTS Dialect("
+                + "dial_id INTEGER PRIMARY KEY,"
+                + "dial STRING NOT NULL)");
+        stm = con.createStatement();
+        stm.execute("CREATE TABLE IF NOT EXISTS Field("
+                + "field_id INTEGER PRIMARY KEY,"
+                + "field STRING NOT NULL)");
+        stm = con.createStatement();
+        stm.execute("CREATE TABLE IF NOT EXISTS Misc("
+                + "misc_id INTEGER PRIMARY KEY,"
+                + "misc STRING NOT NULL)");
+        stm = con.createStatement();
+        stm.execute("CREATE TABLE IF NOT EXISTS DefPos("
+                + "def_id INTEGER NOT NULL,"
+                + "pos_id INTEGER NOT NULL,"
+                + "FOREIGN KEY(def_id) REFERENCES Definition(def_id),"
+                + "FOREIGN KEY(pos_id) REFERENCES PartOfSpeech(pos_id),"
+                + "PRIMARY KEY(def_id, pos_id))");
+        stm = con.createStatement();
+        stm.execute("CREATE TABLE IF NOT EXISTS DefMisc("
+                + "def_id INTEGER NOT NULL,"
+                + "misc_id INTEGER NOT NULL,"
+                + "FOREIGN KEY(def_id) REFERENCES Definition(def_id),"
+                + "FOREIGN KEY(misc_id) REFERENCES Misc(misc_id),"
+                + "PRIMARY KEY(def_id, misc_id))");
+        stm = con.createStatement();
+        stm.execute("CREATE TABLE IF NOT EXISTS DefDial("
+                + "def_id INTEGER NOT NULL,"
+                + "dial_id INTEGER NOT NULL,"
+                + "FOREIGN KEY(def_id) REFERENCES Definition(def_id),"
+                + "FOREIGN KEY(dial_id) REFERENCES Dialect(dial_id),"
+                + "PRIMARY KEY(def_id, dial_id))");
     }
-    
+
     private static File setUpFile() throws IOException {
-        File resFile = new File("testDict"+File.separator+"dbJMDictTest"+File.separator+"dbJMDictTest.json");
+        File resFile = new File("testDict" + File.separator + "dbJMDictTest" + File.separator + "dbJMDictTest.json");
         return resFile;
     }
-    
+
     @org.junit.jupiter.api.BeforeAll
     public static void setUpClass() throws ClassNotFoundException, SQLException, IOException {
         setUpDB();
@@ -54,13 +123,27 @@ public class JMDictParserTest {
     @org.junit.jupiter.api.AfterAll
     public static void tearDownClass() throws IOException, SQLException {
         con.close();
-        File dbFile = new File("testDict"+File.separator+"dbJMDictTest.db");
+        File dbFile = new File("testDict" + File.separator + "dbJMDictTest.db");
         dbFile.delete();
     }
 
     /**
      * Test of doParsing method, of class JMDictParser.
      */
+    @org.junit.jupiter.api.Test
+    public void testDoParsingYieldsCorrectAmount() {
+        int expectedAmount = 9;
+        try {
+            Statement stm = con.createStatement();
+            ResultSet rs = stm.executeQuery("SELECT COUNT(*) as total FROM Word");
+            int actualAmount = rs.getInt("total");
+            assertEquals(expectedAmount, actualAmount);
+        } catch (SQLException | UnsupportedOperationException ex) {
+            Logger.getLogger(IntlinParserTest.class.getName()).log(Level.SEVERE, null, ex);
+            fail("\nException thrown: " + ex.getMessage());
+        }
+    }
+
     @org.junit.jupiter.api.Test
     public void testDoParsingWieldsCorrectWord() throws Exception {
         System.out.println("doParsing");
